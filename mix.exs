69ee38b3e21ec63c503b93356b8046abdbfccdb7
Kip Cole <kipcole9@gmail.com>
2017-11-12 23:44:03 +1100

    Move to cldr version 0.13.0

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule CldrNumbers.Mixfile do
   use Mix.Project
 
-  @version "0.3.2"
+  @version "0.3.3"
 
   def project do
     [
@@ -34,10 +34,10 @@ defmodule CldrNumbers.Mixfile do
 
   defp deps do
     [
-      {:ex_cldr, "~> 0.12.0"},
-      {:decimal, "~> 1.4.1"},
+      {:ex_cldr, "~> 0.13"},
+      {:decimal, "~> 1.4"},
       {:poison, "~> 2.1 or ~> 3.1"},
-      {:ex_doc, ">= 0.18.1", only: :dev}
+      {:ex_doc, "~> 0.18", only: :dev}
     ]
   end
 
