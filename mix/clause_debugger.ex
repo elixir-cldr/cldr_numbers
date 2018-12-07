706b1a24098ec47acb08359d173bd4f898f21ea5
Kip Cole <kipcole9@gmail.com>
2018-12-08 06:57:25 +0800

    Improve clause debugger

--- mix/clause_debugger.ex
+++ mix/clause_debugger.ex
@@ -1,12 +1,27 @@
-defmodule Cldr.Clause do
-  @moduledoc false
+defmodule Cldr.FunctionClause do
+  @moduledoc """
+  Format function clauses using Exception.blame/3
 
-  def functions(module, function, args) do
-    {:ok, kind, clauses} = Exception.blame_mfa(module, function, args)
-    formatted_clauses(function, kind, clauses, &blame_match/2)
+  """
+
+  @doc """
+  Given a `module`, `function`, and `args` see
+  that function clause would match or not match.
+
+  This is useful for helping diagnose function
+  clause errors when many clauses are generated
+  at compile time.
+
+  """
+  @spec match(module(), atom(), [any(), ...]) :: binary()
+  def match(module, function, args) when is_list(args) do
+    case Exception.blame_mfa(module, function, args) do
+      {:ok, kind, clauses} -> formatted_clauses(function, kind, clauses, &blame_match/2)
+      _error -> "No known function clauses match #{inspect module}.#{function}/#{length(args)}"
+    end
   end
 
-  def formatted_clauses(function, kind, clauses, ast_fun) do
+  defp formatted_clauses(function, kind, clauses, ast_fun) do
     format_clause_fun = fn {args, guards} ->
       code = Enum.reduce(guards, {function, [], args}, &{:when, [], [&2, &1]})
       "    #{kind} " <> Macro.to_string(code, ast_fun) <> "\n"
@@ -15,9 +30,13 @@ defmodule Cldr.Clause do
     clauses
     |> Enum.map(format_clause_fun)
     |> Enum.join()
+    |> IO.puts
   end
 
-  defp blame_match(%{match?: true, node: node}, _), do: Macro.to_string(node)
-  defp blame_match(%{match?: false, node: node}, _), do: "-" <> Macro.to_string(node) <> "-"
+  defp blame_match(%{match?: true, node: node}, _),
+    do: Macro.to_string(node)
+  defp blame_match(%{match?: false, node: node}, _),
+    do: IO.ANSI.red() <> Macro.to_string(node) <> IO.ANSI.reset()
   defp blame_match(_, string), do: string
+
 end
