e3c9e91ade6cbf8f3a56b326d6e131d8ed4ffae7
Kip Cole <kipcole9@gmail.com>
2019-03-01 12:10:59 +0100

    Add types and spec to Meta

--- lib/number/format/meta.ex
+++ lib/number/format/meta.ex
@@ -4,6 +4,92 @@ defmodule Cldr.Number.Format.Meta do
   number formatting and provides functions to
   update the struct.
 
+  ## Format definition
+
+  The `:format` is a keyword list that with two
+  elements:
+
+  * `:positive` with is a keyword list for
+    formatting a number >= zero
+
+  * `:negative` which is a keyword list for
+    formatting negtive number
+
+  There are two formats because we can format in
+  an accounting style (that is, numbers surrounded
+  by `()`) or any other arbitrary style. Typically
+  the format for a negative number is the same as
+  that for a positive number with a prepended
+  minus sign.
+
+  ## Localisation of number formatting
+
+  Number formatting is always localised to either
+  the currency processes locale or a locale
+  provided as an option to `Cldr.Number.to_string/3`.
+
+  The metadata is independent of the localisation
+  process. Signs (`+`/`-`), grouping (`,`), decimal markers
+  (`.`) and exponent signs are all localised when
+  the number is formatted.
+
+  ## Formatting directives
+
+  The formats - positive and negative - are defined
+  in the metadata struct, as a keyword list of keywords
+  and values.
+
+  The simplest formatting list might be:
+  ```
+  [format: _]`
+  ```
+  The `:format` keyword indicates
+  that this is where the formatting number will be
+  substituted into the format pattern.
+
+  Another example would be for formatting a negative
+  number:
+  ```
+  [minus: _, format: _]
+  ```
+  which will format with a localised minus sign
+  followed by the formatted number. Note that the
+  keyword value for `:minus` and `:format` are
+  ignored.
+
+  ## List of formatting keywords
+
+  The following is the full list of formatting
+  keywords which can be used to format a
+  number. A `_` in the keyword format is
+  used to denote `:dont_care`.
+
+  * `[format: _]` inserts the formatted number
+    exclusive of any sign
+
+  * `[minus: _]` inserts a localised minus
+    sign
+
+  * `[plus: _]` inserts a localised plus sign
+
+  * `[percent: _]` inserts a localised percent sign
+
+  * `[permille: _]` inserts a localised permille sign
+
+  * `[literal: "string"]` inserts `string` into the
+    format without any processing
+
+  * `[currency: type]` inserts a localised currency
+    symbol of the given `type`.  A `:currency` must be
+    provided as an option to `Cldr.Number.Formatter.Decimal.to_string/3`.
+
+  * `[pad: "char"]` inserts the correct number of `char`
+    to pad the number format to the width specified by
+    `:padding_length` in the `%Meta{}` struct. The `:pad`
+    can be anywhere in the format list but it is most
+    typically inserted before or after the `:format`
+    keyword.
+
   """
   defstruct [
     integer_digits: %{max: 0, min: 1},
@@ -27,11 +113,15 @@ defmodule Cldr.Number.Format.Meta do
     number: 0
   ]
 
+  @typedoc "Metadata type that drives how to format a number"
+  @type t :: %__MODULE__{}
+
   @doc """
   Returns a new number formatting metadata
   struct.
 
   """
+  @spec new :: t()
   def new do
     %__MODULE__{}
   end
@@ -41,6 +131,7 @@ defmodule Cldr.Number.Format.Meta do
   format.
 
   """
+  @spec put_integer_digits(t(), non_neg_integer, non_neg_integer) :: t()
   def put_integer_digits(%__MODULE__{} = meta, min, max \\ 0) when is_integer(min) and is_integer(max) do
     meta
     |> Map.put(:integer_digits, %{min: min, max: max})
@@ -51,6 +142,7 @@ defmodule Cldr.Number.Format.Meta do
   format.
 
   """
+  @spec put_fraction_digits(t(), non_neg_integer, non_neg_integer) :: t()
   def put_fraction_digits(%__MODULE__{} = meta, min, max \\ 0) when is_integer(min) and is_integer(max) do
     meta
     |> Map.put(:fractional_digits, %{min: min, max: max})
@@ -61,6 +153,7 @@ defmodule Cldr.Number.Format.Meta do
   format.
 
   """
+  @spec put_significant_digits(t(), non_neg_integer, non_neg_integer) :: t()
   def put_significant_digits(%__MODULE__{} = meta, min, max \\ 0) when is_integer(min) and is_integer(max) do
     meta
     |> Map.put(:significant_digits, %{min: min, max: max})
@@ -71,6 +164,7 @@ defmodule Cldr.Number.Format.Meta do
   format.
 
   """
+  @spec put_exponent_digits(t(), non_neg_integer) :: t()
   def put_exponent_digits(%__MODULE__{} = meta, digits) when is_integer(digits) do
     meta
     |> Map.put(:exponent_digits, digits)
@@ -81,6 +175,7 @@ defmodule Cldr.Number.Format.Meta do
   the format.
 
   """
+  @spec put_exponent_sign(t(), boolean) :: t()
   def put_exponent_sign(%__MODULE__{} = meta, flag) when is_boolean(flag) do
     meta
     |> Map.put(:exponent_sign, flag)
@@ -91,6 +186,7 @@ defmodule Cldr.Number.Format.Meta do
   be rounded.
 
   """
+  @spec put_integer_digits(t(), non_neg_integer) :: t()
   def put_rounding_digits(%__MODULE__{} = meta, digits) when is_integer(digits) do
     meta
     |> Map.put(:rounding, digits)
@@ -101,11 +197,13 @@ defmodule Cldr.Number.Format.Meta do
   be rounded.
 
   """
+  @spec put_scientific_rounding_digits(t(), non_neg_integer) :: t()
   def put_scientific_rounding_digits(%__MODULE__{} = meta, digits) when is_integer(digits) do
     meta
     |> Map.put(:scientific_rounding, digits)
   end
 
+  @spec put_padding_length(t(), non_neg_integer) :: t()
   def put_padding_length(%__MODULE__{} = meta, digits) when is_integer(digits) do
     meta
     |> Map.put(:padding_length, digits)
@@ -116,6 +214,7 @@ defmodule Cldr.Number.Format.Meta do
   padding the formatted number.
 
   """
+  @spec put_padding_char(t(), String.t()) :: t()
   def put_padding_char(%__MODULE__{} = meta, char) when is_binary(char) do
     meta
     |> Map.put(:padding_char, char)
@@ -129,6 +228,7 @@ defmodule Cldr.Number.Format.Meta do
   formatting as a percent or permille.
 
   """
+  @spec put_multiplier(t(), non_neg_integer) :: t()
   def put_multiplier(%__MODULE__{} = meta, multiplier) when is_integer(multiplier) do
     meta
     |> Map.put(:multiplier, multiplier)
@@ -144,6 +244,7 @@ defmodule Cldr.Number.Format.Meta do
   as the `:locale` option to `to_string/3`.
 
   """
+  @spec put_integer_grouping(t(), non_neg_integer, non_neg_integer) :: t()
   def put_integer_grouping(%__MODULE__{} = meta, first, rest) when is_integer(first) and is_integer(rest) do
     grouping =
       meta
@@ -153,6 +254,7 @@ defmodule Cldr.Number.Format.Meta do
     Map.put(meta, :grouping, grouping)
   end
 
+  @spec put_integer_grouping(t(), non_neg_integer) :: t()
   def put_integer_grouping(%__MODULE__{} = meta, all) when is_integer(all) do
     grouping =
       meta
@@ -172,6 +274,7 @@ defmodule Cldr.Number.Format.Meta do
   as the `:locale` option to `to_string/3`.
 
   """
+  @spec put_fraction_grouping(t(), non_neg_integer, non_neg_integer) :: t()
   def put_fraction_grouping(%__MODULE__{} = meta, first, rest) when is_integer(first) and is_integer(rest) do
     grouping =
       meta
@@ -181,6 +284,7 @@ defmodule Cldr.Number.Format.Meta do
     Map.put(meta, :grouping, grouping)
   end
 
+  @spec put_fraction_grouping(t(), non_neg_integer) :: t()
   def put_fraction_grouping(%__MODULE__{} = meta, all) when is_integer(all) do
     grouping =
       meta
@@ -193,18 +297,20 @@ defmodule Cldr.Number.Format.Meta do
   @doc """
   Set the metadata format.
 
-  Note that this is the parsed format as a simple AST
-  (not the Elixir AST), not a binary representation.
+  Note that this is the parsed format as a simple keyword
+  lsit, not a binary representation.
 
   Its up to each formatting engine to transform its input
   into this form.
 
   """
+  @spec put_format(t(), Keyword.t, Keyword.t) :: t()
   def put_format(%__MODULE__{} = meta, positive_format, negative_format) do
     meta
     |> Map.put(:format, [positive: positive_format, negative: negative_format])
   end
 
+  @spec put_format(t(), Keyword.t) :: t()
   def put_format(%__MODULE__{} = meta, positive_format) do
     put_format(meta, positive_format, [minus: '-', format: :same_as_positive])
   end
