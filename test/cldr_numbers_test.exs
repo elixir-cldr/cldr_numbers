51f91ff65c38eee2bef5f09385bc6663277475f1
Kip Cole <kipcole9@gmail.com>
2019-03-15 23:46:07 +0800

    Run generate doc tests conditionally

--- test/cldr_numbers_test.exs
+++ test/cldr_numbers_test.exs
@@ -1,20 +1,22 @@
 defmodule CldrNumbersTest do
   use ExUnit.Case
 
-  @modules [Number, Number.System, Number.Format, Number.Symbol,
-    Number.Transliterate, Number.Formatter.Decimal, Rbnf.NumberSystem]
+  if function_exported?(Code, :fetch_docs, 1) do
+    @modules [Number, Number.System, Number.Format, Number.Symbol,
+      Number.Transliterate, Number.Formatter.Decimal, Rbnf.NumberSystem]
 
-  test "that no module docs are generated for a backend" do
-    for mod <- @modules do
-      module = Module.concat(NoDoc.Cldr, mod)
-      assert {:docs_v1, _, :elixir, _, :hidden, %{}, _} = Code.fetch_docs(module)
+    test "that no module docs are generated for a backend" do
+      for mod <- @modules do
+        module = Module.concat(NoDoc.Cldr, mod)
+        assert {:docs_v1, _, :elixir, _, :hidden, %{}, _} = Code.fetch_docs(module)
+      end
     end
-  end
 
-  assert "that module docs are generated for a backend" do
-    for mod <- @modules do
-      module = Module.concat(TestBackend.Cldr, mod)
-      {:docs_v1, 1, :elixir, "text/markdown", _, %{}, _} = Code.fetch_docs(module)
+    assert "that module docs are generated for a backend" do
+      for mod <- @modules do
+        module = Module.concat(TestBackend.Cldr, mod)
+        {:docs_v1, 1, :elixir, "text/markdown", _, %{}, _} = Code.fetch_docs(module)
+      end
     end
   end
 end
