e838222d71a4f73c34cc8e095a3720ceb54d47f0
Kip Cole <kipcole9@gmail.com>
2020-04-10 19:42:13 +0800

    Update benchee script

--- bench/to_string.exs
+++ bench/to_string.exs
@@ -1,6 +1,10 @@
+{:ok, options} = Cldr.Number.Format.Options.validate_options([], MyApp.Cldr, locale: Cldr.get_locale())
+
 Benchee.run(
   %{
-    "keyword options" => fn -> MyApp.Cldr.Number.to_string 10000 end,
+    "Number to_string" => fn -> MyApp.Cldr.Number.to_string 10000.55 end,
+    "Number to_string preformatted options" => fn -> MyApp.Cldr.Number.to_string 10000.55, options end,
+    "Float to_string" => fn -> Float.to_string 10000.55 end
   },
   time: 10,
   memory_time: 2
