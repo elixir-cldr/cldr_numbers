ef09fd17a907e16f799b2fcb5ebadf0644f9e8bc
Kip Cole <kipcole9@gmail.com>
2020-05-26 17:45:12 +0800

    Parser takes a string and options

--- lib/cldr/number/parse.ex
+++ lib/cldr/number/parse.ex
@@ -3,10 +3,22 @@ defmodule Cldr.Number.Parser do
   Parse a string into a number and possibly a currency code
 
   """
-  def parse(string, locale, backend) do
+  def parse(string, options \\ []) do
 
 
   end
 
+  defp parse_number(string, locale, backend) do
+    with {:ok, locale} <- Cldr.validate_locale(locale, backend),
+         {:ok, symbols} <- Cldr.Number.Symbol.number_symbols_for(locale, backend) do
+      decimal =
+        string
+        |> String.replace(symbols.latn.group, "")
+        |> String.replace(symbols.latn.decimal, ".")
+        |> Decimal.new()
+
+      {:ok, decimal}
+    end
+  end
 
 end
