1391c4ecd9e2769ac6155b607e88d2887d903b3f
Kip Cole <kipcole9@gmail.com>
2020-10-01 07:03:38 +0800

    Bump ex_cldr dependency to the release version for CLDR38

--- mix.exs
+++ mix.exs
@@ -42,8 +42,7 @@ defmodule CldrNumbers.Mixfile do
   defp deps do
     [
       {:ex_cldr, path: "../cldr", override: true},
-      {:cldr_utils, "~> 2.13"},
-      # {:ex_cldr, "~> 2.15"},
+      # {:ex_cldr, "~> 2.18"},
       {:ex_cldr_currencies, "~> 2.6"},
       {:decimal, "~> 1.6 or ~> 2.0"},
       {:jason, "~> 1.0", optional: true},
