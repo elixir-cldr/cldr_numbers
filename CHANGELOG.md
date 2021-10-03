32d0c1d270d1f630bc17ed7cd8cd5b7acf1415d5
Kip Cole <kipcole9@gmail.com>
2021-10-03 21:13:20 +0800

    Release 2.23.0-rc.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,8 +1,8 @@
 # Changelog
 
-## Cldr_Numbers v2.23.0
+## Cldr_Numbers v2.23.0-rc.0
 
-This is the changelog for Cldr v2.22.1 released on _____, 2021.  For older changelogs please consult the release tag on [GitHub](https://github.com/elixir-cldr/cldr_numbers/tags)
+This is the changelog for Cldr v2.23.0-rc.0 released on October 3rd, 2021.  For older changelogs please consult the release tag on [GitHub](https://github.com/elixir-cldr/cldr_numbers/tags)
 
 ### Enhancements
 
--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule CldrNumbers.Mixfile do
 
   use Mix.Project
 
-  @version "2.23.0"
+  @version "2.23.0-rc.0"
 
   def project do
     [
@@ -41,8 +41,8 @@ defmodule CldrNumbers.Mixfile do
 
   defp deps do
     [
-      {:ex_cldr, path: "../cldr", override: true},
-      {:ex_cldr_currencies, "~> 2.11"},
+      {:ex_cldr, "~> 2.24-rc"},
+      {:ex_cldr_currencies, "~> 2.12-rc"},
       {:decimal, "~> 1.6 or ~> 2.0"},
       {:jason, "~> 1.0", optional: true},
       {:ex_doc, "~> 0.18", optional: true, runtime: false},
