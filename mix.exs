54f4638a52960d4e25e52ed709c1aca96d21447e
Kip Cole <kipcole9@gmail.com>
2022-02-21 06:47:10 +0800

    Update to ex_cldr 2.26.0

--- mix.exs
+++ mix.exs
@@ -41,10 +41,9 @@ defmodule Cldr.Numbers.Mixfile do
 
   defp deps do
     [
-      # {:ex_cldr, "~> 2.24"},
-      {:ex_cldr, path: "../cldr", override: true},
+      {:ex_cldr, "~> 2.26"},
+      {:ex_cldr_currencies, "~> 2.13"},
 
-      {:ex_cldr_currencies, "~> 2.12"},
       {:decimal, "~> 1.6 or ~> 2.0"},
       {:jason, "~> 1.0", optional: true},
       {:ex_doc, "~> 0.18", only: [:dev, :release], optional: true, runtime: false},
