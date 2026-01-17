7631f1aff5daec74f87e703d604c81c39ea41d3a
Kip Cole <kipcole9@gmail.com>
2026-01-18 09:04:39 +1100

    Fix currency code type

--- lib/cldr/number/parse.ex
+++ lib/cldr/number/parse.ex
@@ -364,7 +364,7 @@ defmodule Cldr.Number.Parser do
 
   """
   @spec resolve_currencies([String.t(), ...], Keyword.t()) ::
-          list(Cldr.Currency.code() | String.t())
+          list(Cldr.Currency.currency_code() | String.t())
 
   def resolve_currencies(list, options \\ []) when is_list(list) and is_list(options) do
     resolve(list, &resolve_currency/2, options)
@@ -580,8 +580,8 @@ defmodule Cldr.Number.Parser do
 
   """
   @spec resolve_currency(String.t(), Keyword.t()) ::
-          Cldr.Currency.code()
-          | list(Cldr.Currency.code() | String.t())
+          Cldr.Currency.currency_code()
+          | list(Cldr.Currency.currency_code() | String.t())
           | {:error, {module(), String.t()}}
 
   def resolve_currency(string, options \\ []) when is_binary(string) do
